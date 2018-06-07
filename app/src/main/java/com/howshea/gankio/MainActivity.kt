2acb7bd58e5c51297dd62a174b110fc9765703a8
howshea <howshea@qq.com>
2018-06-07 12:59:39 +0800

    update

--- app/src/main/java/com/howshea/gankio/MainActivity.kt
+++ app/src/main/java/com/howshea/gankio/MainActivity.kt
@@ -3,12 +3,13 @@ package com.howshea.gankio
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.howshea.baseutils.setDarkStatusIcon
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setDarkStatusIcon(false)
+        setDarkStatusIcon(true)
     }
 }
