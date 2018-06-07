35f39ea575caa310c9d6a7d883d2a00883c443b2
howshea <howshea@qq.com>
2018-06-08 07:48:07 +0800

    update UI

--- app/src/main/java/com/howshea/gankio/MainActivity.kt
+++ app/src/main/java/com/howshea/gankio/MainActivity.kt
@@ -6,7 +6,6 @@ import com.howshea.gankio.utils.disableShiftMode
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
