71259d55094495e938d8536a656fdff285587b7d
howshea <howshea@qq.com>
2018-07-18 15:58:08 +0800

    自定义toolbar基本完成

--- basemodule/src/main/java/com/howshea/basemodule/component/SimpleToolbar.kt
+++ basemodule/src/main/java/com/howshea/basemodule/component/SimpleToolbar.kt
@@ -1,17 +1,26 @@
 package com.howshea.basemodule.component
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.graphics.drawable.Drawable
+import android.support.annotation.ColorInt
 import android.support.v7.widget.AppCompatImageButton
+import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import com.howshea.basemodule.R
 import com.howshea.basemodule.extentions.invoke
+import com.howshea.basemodule.extentions.yes
 import com.howshea.basemodule.utils.dip
+import com.howshea.basemodule.utils.sp
 
 /**
  * Created by Howshea
@@ -28,11 +37,21 @@ import com.howshea.basemodule.utils.dip
  */
 
 class SimpleToolbar : FrameLayout {
+    //icon
     private var navButton: ImageButton? = null
     private var navigationDrawable: Drawable? = null
-    private val iconSize = dip(24)
+    private var menuButton: ImageButton? = null
+    private var menuDrawable: Drawable? = null
+    //title
+    private var titleTextView: TextView? = null
+    private var titleText = ""
+    private var titleStyle = 1
+    @ColorInt
+    private var titleColor = Color.parseColor("#656565")
+    private var titleSize = sp(20).toFloat()
     //默认高度48dp
     private var contentHeight = dip(48)
+    private val iconSize = dip(24)
     private val iconTopBottomMargin
         get() = (contentHeight - iconSize) / 2
 
@@ -43,12 +62,21 @@ class SimpleToolbar : FrameLayout {
             ?.apply {
                 if (hasValue(R.styleable.SimpleToolbar_navIcon))
                     navigationDrawable = getDrawable(R.styleable.SimpleToolbar_navIcon)
+
+                if (hasValue(R.styleable.SimpleToolbar_menuIcon))
+                    menuDrawable = getDrawable(R.styleable.SimpleToolbar_menuIcon)
+
                 if (hasValue(R.styleable.SimpleToolbar_contentHeight)) {
                     contentHeight = getDimension(R.styleable.SimpleToolbar_contentHeight, 0f).toInt()
                     (contentHeight < iconSize) {
+                        //contentHeight如果被设置小于icon的高度，就抛出异常
                         throw IllegalArgumentException("contentHeight must be greater than iconSize")
                     }
                 }
+                titleText = getString(R.styleable.SimpleToolbar_title) ?: ""
+                titleStyle = getInt(R.styleable.SimpleToolbar_titleStyle, 1)
+                titleColor = getColor(R.styleable.SimpleToolbar_titleColor, titleColor)
+                titleSize = getDimension(R.styleable.SimpleToolbar_titleSize, titleSize)
                 recycle()
             }
         initSubView()
@@ -58,10 +86,12 @@ class SimpleToolbar : FrameLayout {
 
     private fun initSubView() {
         ensureNavButtonView()
+        ensureMenuButtonView()
+        ensureTitleView()
     }
 
-    private fun addSystemView(v: View) {
-        val vlp = v.layoutParams
+    private fun View.addSystemView() {
+        val vlp = this.layoutParams
         val lp: LayoutParams
         lp = if (vlp == null) {
             generateDefaultLayoutParams() as LayoutParams
@@ -70,24 +100,56 @@ class SimpleToolbar : FrameLayout {
         } else {
             vlp as LayoutParams
         }
-        addView(v, lp)
+        addView(this, lp)
     }
 
     private fun ensureNavButtonView() {
-        (navButton == null && navigationDrawable != null) {
-            navButton = AppCompatImageButton(context, null,
-                R.attr.toolbarNavigationButtonStyle).apply {
-                scaleType = ImageView.ScaleType.FIT_CENTER
-                setImageDrawable(navigationDrawable)
+        navButton ?: let { navigationDrawable != null }
+            .yes {
+                navButton = AppCompatImageButton(context, null,
+                    R.attr.toolbarNavigationButtonStyle).apply {
+                    scaleType = ImageView.ScaleType.FIT_CENTER
+                    setImageDrawable(navigationDrawable)
+                    layoutParams = navButtonLp
+                }.apply {
+                    addSystemView()
+                }
+            }
+    }
+
+    private fun ensureMenuButtonView() {
+        menuButton ?: let { menuDrawable != null }
+            .yes {
+                menuButton = AppCompatImageButton(context, null,
+                    R.attr.toolbarNavigationButtonStyle).apply {
+                    scaleType = ImageView.ScaleType.FIT_CENTER
+                    setImageDrawable(menuDrawable)
+                    layoutParams = menuButtonLp
+                }.apply {
+                    addSystemView()
+                }
+            }
+    }
+
+
+    private fun ensureTitleView() {
+        titleTextView ?: let {
+            titleTextView = AppCompatTextView(context, null).apply {
+                text = titleText
+                setTextColor(titleColor)
+                setTypeface(Typeface.DEFAULT, titleStyle)
+                setTextSize(TypedValue.COMPLEX_UNIT_PX, titleSize)
+                setSingleLine()
+                gravity = Gravity.CENTER
+                layoutParams = getTitleLp(paint)
             }.apply {
-                layoutParams = navButtonLp
-                addSystemView(this)
+                addSystemView()
             }
         }
     }
 
     private val navButtonLp: FrameLayout.LayoutParams by lazy {
-        (generateDefaultLayoutParams() as FrameLayout.LayoutParams).apply {
+        generateDefaultLayoutParams().apply {
             height = iconSize
             width = iconSize
             topMargin = iconTopBottomMargin
@@ -97,8 +159,43 @@ class SimpleToolbar : FrameLayout {
         }
     }
 
-    fun setNavOnClick(click: (v: View) -> Unit) {
-        navButton?.setOnClickListener { click(it) }
+    private val menuButtonLp: FrameLayout.LayoutParams by lazy {
+        generateDefaultLayoutParams().apply {
+            height = iconSize
+            width = iconSize
+            topMargin = iconTopBottomMargin
+            bottomMargin = iconTopBottomMargin
+            marginEnd = dip(12)
+            gravity = Gravity.END
+        }
+    }
+
+    private fun getTitleLp(paint: Paint): FrameLayout.LayoutParams {
+        return generateDefaultLayoutParams().apply {
+            width = paint.measureText(titleText).toInt() + dip(12)
+            gravity = Gravity.CENTER
+        }
+    }
+
+
+    fun setOnNavClick(click: (v: View) -> Unit) {
+        navButton?.setOnClickListener(click)
+    }
+
+    fun setOnMenuClick(click: (v: View) -> Unit) {
+        menuButton?.setOnClickListener(click)
+    }
+
+    fun setTitle(text: CharSequence) {
+        titleTextView?.let {
+            titleText = text.toString()
+            it.layoutParams = getTitleLp(it.paint)
+            it.text = titleText
+        }
+    }
+
+    override fun generateDefaultLayoutParams(): LayoutParams {
+        return LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
     }
 
 }
--- home/src/main/java/com/howshea/home/ui/HomeFragment.kt
+++ home/src/main/java/com/howshea/home/ui/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.Toast
 import com.howshea.basemodule.component.LazyFragment
 import com.howshea.basemodule.extentions.topPadding
@@ -28,8 +27,11 @@ class HomeFragment : LazyFragment() {
 
     override fun initView() {
         toolbar.topPadding = activity?.getStatusBarHeight() ?: 0
-        toolbar.setNavOnClick {
-            Toast.makeText(context,"啦啦啦",Toast.LENGTH_SHORT).show()
+        toolbar.setOnNavClick {
+            Toast.makeText(context, "navigationIcon", Toast.LENGTH_SHORT).show()
+        }
+        toolbar.setOnMenuClick {
+            toolbar.setTitle("Google")
         }
     }
 }
