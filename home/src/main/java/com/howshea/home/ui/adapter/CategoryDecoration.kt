f64385927709bb5b71e60678317dbd5628a94e89
howshea <howshea@qq.com>
2018-09-10 15:30:21 +0800

    完成标签绘制

--- home/src/main/java/com/howshea/home/ui/adapter/CategoryDecoration.kt
+++ home/src/main/java/com/howshea/home/ui/adapter/CategoryDecoration.kt
@@ -3,9 +3,7 @@ package com.howshea.home.ui.adapter
 import android.content.Context
 import android.graphics.*
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
-import com.howshea.basemodule.AppContext
 import com.howshea.basemodule.utils.dp
 import com.howshea.home.R
 import com.howshea.home.model.Common
@@ -19,25 +17,34 @@ class CategoryDecoration(val data: List<Common>, context: Context) : RecyclerVie
         color = Color.WHITE
         isAntiAlias = true
     }
-    private val textPaint = Paint().apply {
-        isAntiAlias = true
-        color = context.resources.getColor(R.color.colorPrimary)
-        textSize = context.dp(11).toFloat()
-        textAlign = Paint.Align.LEFT
+    private val textPaint by lazy {
+        Paint().apply {
+            isAntiAlias = true
+            color = context.resources.getColor(R.color.colorPrimary)
+            textSize = context.dp(12).toFloat()
+            textAlign = Paint.Align.LEFT
+        }
+    }
+    private val tagPaint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = context.resources.getColor(R.color.bg_tag)
+        }
     }
     private var dividerHeight = context.dp(9)
-    private val tagHeight = context.dp(38)
+    private val tagBgHeight = context.dp(38)
+    private val tagHeight = context.dp(22)
+    private val tagPadding = context.dp(12.5f)
     private val lineHeight = context.dp(0.7f)
     private val paddingLeft = context.dp(16)
-    private val radius = context.dp(9)
+    private val radius = context.dp(11)
 
     override fun getItemOffsets(outRect: Rect?, view: View, parent: RecyclerView, state: RecyclerView.State?) {
         super.getItemOffsets(outRect, view, parent, state)
         val position = parent.getChildAdapterPosition(view)
         if (position < 0) return
         outRect?.top = when {
-            position == 0 -> tagHeight
-            isFirstInGroup(position) -> tagHeight + dividerHeight
+            position == 0 -> tagBgHeight
+            isFirstInGroup(position) -> tagBgHeight + dividerHeight
             else -> lineHeight
         }
     }
@@ -49,22 +56,22 @@ class CategoryDecoration(val data: List<Common>, context: Context) : RecyclerVie
             val child = parent.getChildAt(it)
             val index = parent.getChildAdapterPosition(child)
             when {
-
                 isFirstInGroup(index) -> {
-                    val top = child.top - tagHeight.toFloat()
+                    //绘制白色矩形背景
+                    val top = child.top - tagBgHeight.toFloat()
                     val bottom = child.top - lineHeight.toFloat()
                     c.drawRect(0f, top, parent.width.toFloat(), bottom, paint)
+                    //绘制 Group 标签
                     val text = data[index].type
                     val textWidth = textPaint.measureText(text).toInt()
-                    val rectF = RectF(paddingLeft.toFloat(), top + AppContext.dp(10), paddingLeft + textWidth + AppContext.dp(13.5f) * 2.0f, top + AppContext.dp(28))
-                    c.drawRoundRect(rectF, radius.toFloat(), radius.toFloat(), Paint(Paint.ANTI_ALIAS_FLAG).apply {
-                        color = Color.parseColor("#F0F3F5")
-                    })
+                    //这里用 child.top 而不用 bottom，是为了让字体视觉上更居中
+                    val tagTop = top + (child.top - top - tagHeight) / 2f
+                    val rectF = RectF(paddingLeft.toFloat(), tagTop, paddingLeft + textWidth + tagPadding * 2.0f, tagTop + tagHeight)
+                    c.drawRoundRect(rectF, radius.toFloat(), radius.toFloat(), tagPaint)
                     val textHeight = textPaint.fontMetrics.bottom - textPaint.fontMetrics.top
-                    val baseLine = rectF.top + (rectF.height() - textHeight) / 2.0f - textPaint.fontMetrics.top
-                    val textLeft = rectF.left + AppContext.dp(13.5f).toFloat()
+                    val baseLine = top + (bottom - top - textHeight) / 2.0f - textPaint.fontMetrics.top
+                    val textLeft = rectF.left + tagPadding.toFloat()
                     c.drawText(text, textLeft, baseLine, textPaint)
-
                 }
                 else -> {
                     val top = (child.top - lineHeight).toFloat()
