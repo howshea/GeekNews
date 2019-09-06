df6fba3e061c912ee53f065b8d8572e3fa98bdef
howshea <howshea@qq.com>
2019-09-06 15:17:53 +0800

    Migrate build.gradle of app

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    kotlin("android.extensions")
+}
+
+
+android {
+    compileSdkVersion(Build.targetSdk)
+    defaultConfig {
+        applicationId = "com.howshea.geeknews"
+        minSdkVersion(Build.minSdk)
+        targetSdkVersion(Build.targetSdk)
+        versionCode = Build.versionCode
+        versionName = Build.versionName
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isZipAlignEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    dataBinding.isEnabled = true
+    packagingOptions {
+        exclude("META-INF/rxjava.properties")
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    //module
+    implementation(project(":basemodule"))
+    implementation(project(":home"))
+    implementation(project(":data"))
+    implementation(project(":read"))
+    implementation(project(":personalcenter"))
+    //test
+    testImplementation(Deps.Test.junit)
+    androidTestImplementation(Deps.Test.runner)
+    androidTestImplementation(Deps.Test.espresso)
+}
